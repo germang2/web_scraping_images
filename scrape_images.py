ec087b3ee3df40d45dc68c61685de01ce7ac88e5
German Gomez <germandg99@gmail.com>
2020-04-06 21:08:23 -0500

    Requesting url image and saving it in disk

--- scrape_images.py
+++ scrape_images.py
@@ -3,8 +3,13 @@ import json
 
 url = "https://dog.ceo/api/breeds/image/random"
 
+
 r = requests.get(url)
 
 if r.status_code == 200:
     data = json.loads(r.text)
-    print(data)
+    url_image = data['message']
+    req_image = requests.get(url_image)
+    if req_image.status_code == 200:
+        with open('images/dog_image.jpg', 'wb') as image:
+            image.write(req_image.content)
