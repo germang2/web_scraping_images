dd266df8c0474cecc2120a02d324e10af04da8ff
German Gomez <germandg99@gmail.com>
2020-04-06 21:12:05 -0500

    Loop for retrieve and save images

--- scrape_images.py
+++ scrape_images.py
@@ -4,12 +4,17 @@ import json
 url = "https://dog.ceo/api/breeds/image/random"
 
 
-r = requests.get(url)
-
-if r.status_code == 200:
-    data = json.loads(r.text)
-    url_image = data['message']
-    req_image = requests.get(url_image)
-    if req_image.status_code == 200:
-        with open('images/dog_image.jpg', 'wb') as image:
-            image.write(req_image.content)
+for i in range(1, 6):
+    try:
+        r = requests.get(url)
+        if r.status_code == 200:
+            data = json.loads(r.text)
+            url_image = data['message']
+            req_image = requests.get(url_image)
+            if req_image.status_code == 200:
+                with open(f'images/dog_image{i}.jpg', 'wb') as image:
+                    image.write(req_image.content)
+                    print(f'image number {i} written successfully')
+    except Exception as exp:
+        print(f'Fail getting image number {i}')
+        print(exp)
